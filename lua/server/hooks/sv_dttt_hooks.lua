cf4c8339d5029f14ed5217374917a4747059e25e
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-23 14:18:05 +0100

    Feat(Hooks): Added a few more hooks

--- lua/server/hooks/sv_dttt_hooks.lua
+++ lua/server/hooks/sv_dttt_hooks.lua
@@ -1,4 +1,5 @@
 include("shared/sh_globals.lua")
+include("server/utils/sv_general.lua")
 include("server/utils/sv_muter.lua")
 
 -- Runs muting logic
@@ -17,4 +18,25 @@ end)
 
 hook.Add(HOOKS.UNMUTE_ALL_PLAYERS, "DiscordUnmuteAllPlayers", function()
     unmuteAllPlayers()
+end)
+
+hook.Add(HOOKS.GET_DISCORD_IDS, "DTTTGetDiscordIDs", function()
+    return _G.dttt_id_mapping
+end)
+
+hook.Add(HOOKS.GET_MUTED_PLAYERS, "DTTTGetMutedPlayers", function()
+    return _G.dttt_muted_players
+end)
+
+hook.Add(HOOKS.GET_DISCORD_ID, "DTTTGetDiscordID", function(ply)
+    local discord_id = _G.dttt_muted_players[playerIdToString(ply)]
+    return discord_id
+end)
+
+hook.Add(HOOKS.GET_MUTE_STATUS, "DTTTGetMuteStatus", function(ply)
+    return getMuteStatus(ply)
+end)
+
+hook.Add(HOOKS.SET_INTERNAL_MUTE_LOGIC, "DTTTSetInternalMuteLogic", function(enabled)
+    setInternalMuteLogicEnabled(enabled)
 end)
