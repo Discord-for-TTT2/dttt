017f72d0c829c6fcee6600941e81cf89fdb2f7fd
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-24 15:46:45 +0100

    Added more commands

--- lua/server/dttt_commands.lua
+++ lua/server/dttt_commands.lua
@@ -22,3 +22,46 @@ addCommand("dttt_mappings", function(ply, cmd, args, argStr)
         forceLog("SteamID:" .. tostring(key) .. " ; DiscordID:" .. tostring(value))
     end
 end)
+
+addCommand("dttt_player_states", function(ply, cmd, args, argStr)
+
+    forceLog("Muted Players: ")
+    for key, value in pairs(g_dttt_player_states.muted) do
+        forceLog("SteamID:" .. key .. " ; State:" .. tostring(value))
+    end
+
+    forceLog("Deafened Players: ")
+    for key, value in pairs(g_dttt_player_states.deafened) do
+        forceLog("SteamID:" .. key .. " ; State:" .. tostring(value))
+    end
+
+    forceLog("")
+end)
+
+addCommand("dttt_clear_mapping", function(ply, cmd, args, argStr)
+    clearDiscordIds()
+end)
+
+addCommand("dttt_run_automapper", function(ply, cmd, args, argStr)
+    local players = player.GetHumans()
+
+    for _, ply in ipairs(players) do
+        autoMapId(ply)
+    end
+end)
+
+addCommand("dttt_mute_all", function(ply, cmd, args, argStr)
+    hook.Run("DTTTMuteAll")
+end)
+
+addCommand("dttt_unmute_all", function(ply, cmd, args, argStr)
+    hook.Run("DTTTUnmuteAll")
+end)
+
+addCommand("dttt_deafen_all", function(ply, cmd, args, argStr)
+    hook.Run("DTTTDeafenAll")
+end)
+
+addCommand("dttt_undeafen_all", function(ply, cmd, args, argStr)
+    hook.Run("DTTTUndeafenAll")
+end)
