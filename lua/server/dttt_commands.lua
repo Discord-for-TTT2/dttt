fea922c092f29d3ff9284cea71dd465ef6ab95b0
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-24 13:33:15 +0100

    Added new command to list vars and all commands

--- lua/server/dttt_commands.lua
+++ lua/server/dttt_commands.lua
@@ -1,5 +1,17 @@
-concommand.Add("dttt", function(ply, cmd, args, argStr)
-    print(#g_convars)
+local commands = {}
+
+local function addCommand(name, callback)
+    table.insert(commands, name)
+    concommand.Add(name, callback)
+end
+
+addCommand("dttt", function(ply, cmd, args, argStr)
+    for _, command in ipairs(commands) do
+        forceLog(command)
+    end
+end)
+
+addCommand("dttt_vars", function(ply, cmd, args, argStr)
     for _, convar in ipairs(g_convars) do
         forceLog(convar:GetName() .. " : " .. convar:GetString())
     end
