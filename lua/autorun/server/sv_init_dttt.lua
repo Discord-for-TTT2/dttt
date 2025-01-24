88825886dc92a8c1f812b45a656bd481c5d7cc08
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-24 13:13:17 +0100

    Made convars actually save

--- lua/autorun/server/sv_init_dttt.lua
+++ lua/autorun/server/sv_init_dttt.lua
@@ -13,26 +13,26 @@ g_dttt_discord_mapping = {}
 --- Create Con Vars ---
 
 -- Debug
-CreateConVar("dttt_dbg_enabled", "0")
-CreateConVar("dttt_dbg_timestamp_enabled", "1")
-CreateConVar("dttt_dbg_log_levels", "WARNING|ERROR")
+CreateConVar("dttt_dbg_enabled", "0", {FCVAR_ARCHIVE})
+CreateConVar("dttt_dbg_timestamp_enabled", "1", {FCVAR_ARCHIVE})
+CreateConVar("dttt_dbg_log_levels", "WARNING|ERROR", {FCVAR_ARCHIVE})
 
 -- Muting
-CreateConVar("dttt_imute_logic_enabled", "1")
-CreateConVar("dttt_imute_enabled", "1")
-CreateConVar("dttt_iunmute_enabled", "1")
+CreateConVar("dttt_imute_logic_enabled", "1", {FCVAR_ARCHIVE})
+CreateConVar("dttt_imute_enabled", "1", {FCVAR_ARCHIVE})
+CreateConVar("dttt_iunmute_enabled", "1", {FCVAR_ARCHIVE})
 
 -- Durations
-CreateConVar("dttt_mute_duration", "5")
-CreateConVar("dttt_deafen_duration", "5")
+CreateConVar("dttt_mute_duration", "5", {FCVAR_ARCHIVE, FCVAR_NOTIFY})
+CreateConVar("dttt_deafen_duration", "5", {FCVAR_ARCHIVE, FCVAR_NOTIFY})
 
 -- Bot
-CreateConVar("dttt_bot_endpoint", "http://localhost:43507")
-CreateConVar("dttt_bot_api_key", "")
+CreateConVar("dttt_bot_endpoint", "http://localhost:43507", {FCVAR_ARCHIVE})
+CreateConVar("dttt_bot_api_key", "", {FCVAR_ARCHIVE})
 
 -- Discord
-CreateConVar("dttt_auto_map_ids", "1")
-CreateConVar("dttt_cache_mapping", "1")
+CreateConVar("dttt_auto_map_ids", "1", {FCVAR_ARCHIVE})
+CreateConVar("dttt_cache_mapping", "1", {FCVAR_ARCHIVE})
 
 
 --- Include needed files ---
