dd687bc64600cfc53812403c1ad04b7a53d5ef80
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-24 13:22:47 +0100

    Printing length of convar table for debugging

--- lua/server/dttt_commands.lua
+++ lua/server/dttt_commands.lua
@@ -1,4 +1,5 @@
 concommand.Add("dttt", function(ply, cmd, args, argStr)
+    print(#g_convars)
     for _, convar in ipairs(g_convars) do
         forceLog(convar:GetName() .. " : " .. convar:GetString())
     end
