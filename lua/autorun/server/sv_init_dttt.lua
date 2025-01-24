857494288b77d548a57aab1f5d21a6efa3846877
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-24 16:13:10 +0100

    Added helptext to convars

--- lua/autorun/server/sv_init_dttt.lua
+++ lua/autorun/server/sv_init_dttt.lua
@@ -20,26 +20,26 @@ local function RegisterConVar(name, value, flags, description)
 end
 
 -- Debug
-RegisterConVar("dttt_dbg_enabled", "0", {FCVAR_ARCHIVE})
-RegisterConVar("dttt_dbg_timestamp_enabled", "1", {FCVAR_ARCHIVE})
-RegisterConVar("dttt_dbg_log_levels", "WARNING|ERROR", {FCVAR_ARCHIVE})
+RegisterConVar("dttt_dbg_enabled", "0", {FCVAR_ARCHIVE}, "Enabled DTTT Logging to the Console")
+RegisterConVar("dttt_dbg_timestamp_enabled", "1", {FCVAR_ARCHIVE}, "If enabled adds a timestamp to log messages")
+RegisterConVar("dttt_dbg_log_levels", "WARNING|ERROR", {FCVAR_ARCHIVE}, "Sets the log message levels. Available: INFO,DEBUG,WARNING,ERROR")
 
 -- Muting
-RegisterConVar("dttt_imute_logic_enabled", "1", {FCVAR_ARCHIVE})
-RegisterConVar("dttt_imute_enabled", "1", {FCVAR_ARCHIVE})
-RegisterConVar("dttt_iunmute_enabled", "1", {FCVAR_ARCHIVE})
+RegisterConVar("dttt_imute_logic_enabled", "1", {FCVAR_ARCHIVE}, "If disabled the internal all mute logic needs to be handled manually (Muting/Unmuting)")
+RegisterConVar("dttt_imute_enabled", "1", {FCVAR_ARCHIVE}, "If disabled internal muting does not work and needs to be handled manually")
+RegisterConVar("dttt_iunmute_enabled", "1", {FCVAR_ARCHIVE}, "If disabled internal unmuting does not work and needs to be handled manually")
 
 -- Durations
-RegisterConVar("dttt_mute_duration", "5", {FCVAR_ARCHIVE, FCVAR_NOTIFY})
-RegisterConVar("dttt_deafen_duration", "5", {FCVAR_ARCHIVE, FCVAR_NOTIFY})
+RegisterConVar("dttt_mute_duration", "5", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "The duration a player is muted. Use 0 to mute for the entire round")
+RegisterConVar("dttt_deafen_duration", "5", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "The duration a player is deafened. Use 0 to deafen for the entire round")
 
 -- Bot
-RegisterConVar("dttt_bot_endpoint", "http://localhost:37405", {FCVAR_ARCHIVE})
-RegisterConVar("dttt_bot_api_key", "", {FCVAR_ARCHIVE})
+RegisterConVar("dttt_bot_endpoint", "http://localhost:37405", {FCVAR_ARCHIVE}, "The endpoint of the bot")
+RegisterConVar("dttt_bot_api_key", "", {FCVAR_ARCHIVE}, "The api key for the bot, will be sent in the Authorization Header")
 
 -- Discord
-RegisterConVar("dttt_auto_map_ids", "1", {FCVAR_ARCHIVE})
-RegisterConVar("dttt_cache_mapping", "1", {FCVAR_ARCHIVE})
+RegisterConVar("dttt_auto_map_ids", "1", {FCVAR_ARCHIVE}, "If disabled dttt wont try to automatically get the discord ids for players")
+RegisterConVar("dttt_cache_mapping", "1", {FCVAR_ARCHIVE}, "If disabled dttt wont cache the discord ids, this means the ids need to get added again after a restart")
 
 
 --- Include needed files ---
