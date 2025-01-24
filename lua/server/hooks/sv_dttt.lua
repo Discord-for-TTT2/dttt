68fccd3f84bff6c8862a12cee5632a91f2fb29d1
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-24 15:23:12 +0100

    Now running hooks when player gets muted/deafened

--- lua/server/hooks/sv_dttt.lua
+++ lua/server/hooks/sv_dttt.lua
@@ -2,48 +2,56 @@ hook.Add("DTTTMute", "dttt_mute", function(ply, duration)
 -- mute player
     duration = duration or 0
     mutePlayer(ply, duration)
+    hook.Run("DTTTPlayerMuted", ply, true)
 end)
 
 hook.Add("DTTTUnmute", "dttt_unmute", function(ply, duration)
 -- unmute player
     duration = duration or 0
     unmutePlayer(ply, duration)
+    hook.Run("DTTTPlayerMuted", ply, false)
 end)
 
 hook.Add("DTTTMuteAll", "dttt_mute_all", function(state, duration)
     -- mute all
     duration = duration or 0
     muteAll(duration)
+    hook.Run("DTTTAllMuted", true)
 end)
 
 hook.Add("DTTTUnmuteAll", "dttt_unmute_all", function(duration)
 -- unmute all
     duration = duration or 0
     unmuteAll(duration)
+    hook.Run("DTTTAllMuted", false)
 end)
 
 hook.Add("DTTTDeafen", "dttt_deafen", function(ply, duration)
 -- deafen player
     duration = duration or 0
     deafenPlayer(ply, duration)
+    hook.Run("DTTTPlayerDefened", ply, true)
 end)
 
 hook.Add("DTTTUndeafen", "dttt_undeafen", function(ply, duration)
 -- undeafen player
     duration = duration or 0
     undeafenPlayer(ply, duration)
+    hook.Run("DTTTPlayerDefened", ply, false)
 end)
 
 hook.Add("DTTTDeafenAll", "dttt_deafen_all", function(duration)
 -- deafen all
     duration = duration or 0
     deafenAll(duration)
+    hook.Run("DTTTAllDeafened", true)
 end)
 
 hook.Add("DTTTUndeafenAll", "dttt_undeafen_all", function(duration)
 -- undeafen all
     duration = duration or 0
     undeafenAll(duration)
+    hook.Run("DTTTAllDeafened", false)
 end)
 
 --[[
@@ -60,34 +68,32 @@ end)
 
 hook.Add("DTTTGetMuted", "dttt_get_muted", function()
 -- return muted players
+    return g_dttt_player_states.muted
 end)
 
 hook.Add("DTTTGetDeafened", "dttt_get_deafened", function()
 -- return deafened player
+    return g_dttt_player_states.deafened
 end)
 
 hook.Add("DTTTGetDiscordIDs", "dttt_get_discord_ids", function()
 -- get all discord id mappings
+    return g_dttt_discord_mapping
 end)
 
 hook.Add("DTTTGetMuteState", "dttt_get_mute_state", function(ply)
 -- get mute state of player
+    return getMuteState(ply)
 end)
 
 hook.Add("DTTTGetDeafenedState", "dttt_get_deafened_state", function(ply)
 -- get deafen state of player
+    return getDeafenState(ply)
 end)
 
 hook.Add("DTTTGetDiscordID", "dttt_get_discord_id", function(ply)
 -- get discord id mapping of player
-end)
-
-hook.Add("DTTTPlayerMuted", "dttt_player_muted", function()
--- run when player got muted
-end)
-
-hook.Add("DTTTPlayerDefened", "dttt_player_defened", function()
--- run when player got deafened
+    return getMappedId(ply)
 end)
 
 hook.Add("DTTTPlayerMoved", "dttt_player_moved", function()
