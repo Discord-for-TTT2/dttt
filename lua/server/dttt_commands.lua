84f5b11528e7407e42f5dfb0f9e1b027214b1425
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-24 13:13:38 +0100

    Updated dttt command

--- lua/server/dttt_commands.lua
+++ lua/server/dttt_commands.lua
@@ -1,4 +1,13 @@
 concommand.Add("dttt", function(ply, cmd, args, argStr)
-    logInfo("DTTT EXECUTED")
-    print("DISCORDO")
+    for _, convarName in ipairs(engine.GetConsoleCommands()) do
+        local convar = GetConVar(convarName)
+        if not convar then
+            return
+        end
+
+        -- Check if the ConVar name starts with a certain prefix
+        if string.StartWith(convarName, "dttt_") then
+            forceLog(convar:GetName() .. " : " .. convar:GetString())
+        end
+    end
 end)
