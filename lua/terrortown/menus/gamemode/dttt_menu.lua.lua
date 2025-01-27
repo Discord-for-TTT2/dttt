37bdb59638e0293bb94b95c852fdc931bf6bef81
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-27 12:42:21 +0100

    Removed hook and tried adding a menu

---
+++ lua/terrortown/menus/gamemode/dttt_menu.lua.lua
@@ -0,0 +1,12 @@
+local tableCopy = table.copy
+
+CLGAMEMODEMENU.base = "base_gamemodemenu"
+
+CLGAMEMODEMENU.title =  "Test"
+CLGAMEMODEMENU.description = "Desc"
+
+CLGAMEMODEMENU.isInitialised = false
+
+function CLGAMEMODEMENU:IsAdminMenu()
+    return true
+end
